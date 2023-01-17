a00c034c91804dfcaca1f118971d1bd43d9c9bc1
guilhermehub12 <guilhermee1312@hotmail.com>
2023-01-17 00:52:25 -0300

    build: list of musics

---
+++ index.js
@@ -0,0 +1,27 @@
+let musics = [
+  {
+    title: "Lofi Beat",
+    artist: "Lofi Musics",
+    src: "files/lofi-beat.mp3",
+    img: "files/bg.webp",
+  },
+  {
+    title: "Lofi Hip Hop",
+    artist: "Lofi Musics",
+    src: "files/lofi-hip-hop.mp3",
+    img: "files/japan-bg.webp",
+  },
+  {
+    title: "Lofi Study",
+    artist: "Lofi Musics",
+    src: "files/lofi-study.mp3",
+    img: "files/code-bg.webp",
+  },
+  {
+    title: "Sunset Vibes",
+    artist: "Lofi Musics",
+    src: "files/sunset-vibes.mp3",
+    img: "files/cyber-bg.webp",
+  },
+];
+
