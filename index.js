c7e52993576b78ff7af3fd9094a85a42501c2b5d
guilhermehub12 <guilhermee1312@hotmail.com>
2023-01-17 01:17:39 -0300

    fix: change name directory

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ let musics = [
   {
     title: "Lofi Beat",
     artist: "Lofi Musics",
-    src: "files/lofi-beat.mp3",
-    img: "files/bg.webp",
+    src: "assets/lofi-beat.mp3",
+    img: "assets/bg.webp",
   },
   {
     title: "Lofi Hip Hop",
     artist: "Lofi Musics",
-    src: "files/lofi-hip-hop.mp3",
-    img: "files/japan-bg.webp",
+    src: "assets/lofi-hip-hop.mp3",
+    img: "assets/japan-bg.webp",
   },
   {
     title: "Lofi Study",
     artist: "Lofi Musics",
-    src: "files/lofi-study.mp3",
-    img: "files/code-bg.webp",
+    src: "assets/lofi-study.mp3",
+    img: "assets/code-bg.webp",
   },
   {
     title: "Sunset Vibes",
     artist: "Lofi Musics",
-    src: "files/sunset-vibes.mp3",
-    img: "files/cyber-bg.webp",
+    src: "assets/sunset-vibes.mp3",
+    img: "assets/cyber-bg.webp",
   },
 ];
 
@@ -102,6 +102,6 @@ function secondsToMinutes(seconds) {
   if (campoSegundos < 10) {
     campoSegundos = "0" + campoSegundos;
   }
-  
+
   return campoMinutos + ":" + campoSegundos;
 }
